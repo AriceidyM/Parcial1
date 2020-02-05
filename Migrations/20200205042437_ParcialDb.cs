7a9f73c3c9f9aa15026d3932bb346f6fc4454476
Ariceidy Maria Ure�a Rosario <ariceidy_urena@ucne.edu.do>
2020-02-05 00:32:36 -0400

    Add project files.

---
+++ Migrations/20200205042437_ParcialDb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rParcial.Migrations
+{
+    public partial class ParcialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usuario",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuario", x => x.UsuarioId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usuario");
+        }
+    }
+}
